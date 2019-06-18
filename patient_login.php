b638c87dbf757a1e5d0c1852d1e63cfff6f67a02
Dipolelo <dipolelodips@gmail.com>
2019-06-18 15:24:19 +0200

    Improved front-end layout

--- patient_login.php
+++ patient_login.php
@@ -35,7 +35,7 @@
                   </div-->
                 <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit" id="login" name="login">LOGIN</button>
                 <div class="font">
-                <a class="small" href="forgot--password.html"><strong><center>Forgot password?</center></strong></a></div>
+                <a class="small" href="forgot_password.html"><strong><center>Forgot password?</center></strong></a></div>
                 <center><p>Don't have an account? <a class="small" href="patient_register.php"><strong>Register</strong></a></p></center>                    
               </form>
             </div>
