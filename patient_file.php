f811c2e9442e82032cc8d1d822f59a665571ec02
Dipolelo <dipolelodips@gmail.com>
2019-06-20 08:32:45 +0200

    Fixed href typo

--- patient_file.php
+++ patient_file.php
@@ -276,7 +276,7 @@
 
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="partience-login.html" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <a class="nav-link dropdown-toggle" href="patient_login.php" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Amanda</span>
                 <img class="img-profile rounded-circle" src="img/images.jpg">
               </a>
--- patient_profile.php
+++ patient_profile.php
@@ -287,7 +287,7 @@
 
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="partience-login.html" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <a class="nav-link dropdown-toggle" href="patient_login.php" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Amanda</span>
                 <img class="img-profile rounded-circle" src="img/images.jpg">
               </a>
@@ -298,7 +298,7 @@
                   Profile
                 </a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="partience-login.html" data-toggle="modal" data-target="#logoutModal">
+                <a class="dropdown-item" href="patient_login.php" data-toggle="modal" data-target="#logoutModal">
                   <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                   Logout
                 </a>
@@ -425,7 +425,7 @@
         <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
         <div class="modal-footer">
           <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="partience-login.html">Logout</a>
+          <a class="btn btn-primary" href="patient_login.php">Logout</a>
         </div>
       </div>
     </div>
