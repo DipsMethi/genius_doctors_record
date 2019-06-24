84b2916819c3697a37b073e264e1853773961d2d
Dipolelo <dipolelodips@gmail.com>
2019-06-24 08:17:30 +0200

    Reconfigured the router

--- router.php
+++ router.php
@@ -1,46 +1,45 @@
 
 <?php
-    // Start session
-    if(session_start())
+    try
     {
-        $_SESSION['id'] = $_POST['id'];
+        if( isValid($_POST['id'], $_POST['pswd']) )
+            header("location: patient_dashboard.php");
+        else
+            alert("Invalid login credentials.");
     }
-
+    catch(Exception $e)
+    {
+        alert($e);
+    }
+    
     // Displays message box with $msg
     function alert($msg)
     {
         echo "<script type='text/javascript'> alert('$msg'); </script>";
     }
 
-    // Authenticates login credentials
-    function Authenticate()
+    function isValid($id, $pswd)
     {
-        $flag = false;
-
-        $_idNum = $_POST['id'];
-        $_password = $_POST['pswd'];
-
-        $connectionString = mysqli_connect("localhost", "root", "", "doctors_db");
-        
-        $result = mysqli_query($connectionString, "SELECT idNum,pswd FROM patient_profile WHERE idNum='$_idNum' && pswd='$_password'");
-        if(!$result) die("Fatal error");
-
-        $rows = mysqli_num_rows($result);
-        if( $rows > 0 )
+        try
         {
-            $_SESSION['id'] = $_idNum;
-            $flag = true;
+            $conStr = mysqli_connect("localhost","root","","doctors_db");
+            $res = null;
+            $result = mysqli_query($conStr,"SELECT idNum,fst_name,lst_name,email 
+                                FROM patient_profile 
+                                WHERE idNum='$id' && pswd='$pswd'");
+            
+            if(mysqli_num_rows($result) > 0)
+                return true;
+            else
+                return false;
         }
-        return $flag;
-    }
-
-    if( Authenticate() )
-    {
-        // Redirects to patient_dashboard
-        header("location: patient_dashboard.php");
-    }
-    else
-    {
-        alert("Invalid login credentials.");
+        catch(Exception $e)
+        {
+            echo "<script> alert($e); </script>";
+        }
+        finally
+        {
+            mysqli_close($conStr);
+        }
     }
 ?>
