0f13a58157b18c264a578ff408c4e3c1b03eb503
Dipolelo <dipolelodips@gmail.com>
2019-06-25 12:08:36 +0200

    Fixed Syntax

--- Utilities/JSUtil.php
+++ Utilities/JSUtil.php
@@ -2,11 +2,11 @@
 <?php 
     function alert($message)
     {
-        echo "<script> alert($message) </script>";
+        echo "<script> alert('$message'); </script>";
     }
 
     function route($link)
     {
-        echo "<script> window.location.href = $link</script>";
+        echo "<script> window.location.href = '$link';</script>";
     }
 ?>
---
+++ Utilities/loginManager.php
@@ -0,0 +1,82 @@
+<?php
+    require_once "Utilities/JSUtil.php";
+    // This class needs refactoring
+    class Session
+    {
+        public $id;
+        public $pswd;
+        public $firstName;
+        public $lastName;
+        public $email;
+        
+        // Session contructor
+        function __construct($id, $pswd)
+        {
+            $this->$id = $id;
+            $this->$pswd = $pswd;
+        }
+
+        // Session destructor
+        function __destruct()
+        {
+            $this->destroy_session();
+        }
+
+        // Begins the session
+        function login($id,$pswd)
+        {
+            try
+            {
+                $isLoggedIn = false;
+                $profile = $this->getProfile($id,$pswd);
+                
+                if( !isset($_SESSION['id']) )
+                {
+                    // Store session variables
+                    $_SESSION['id'] = $profile[0];
+                    $_SESSION['fstName'] = $profile[1];
+                    $_SESSION['lstName'] = $profile[2];
+                    $_SESSION['email'] = $profile[3];
+                    
+                    $isLoggedIn = true;
+                }
+                return $isLoggedIn;
+            }
+            catch(Exception $e)
+            {
+                throw $e;
+            }
+        }
+
+        // Ends the session
+        private function destroy_session()
+        {
+            $_SESSION = array(); // Empty session variables
+            session_destroy(); // Destroy current session
+        }
+
+        // Gets user profile from patient_profile table
+        // Returns the record where idNum=$id
+        private function getProfile($id,$pswd)
+        {
+            try
+            {
+                $conStr = mysqli_connect("localhost","root","","doctors_db");
+                $result = mysqli_query($conStr,"SELECT idNum,fst_name,lst_name,email 
+                                    FROM patient_profile 
+                                    WHERE idNum='$id' && pswd='$pswd'");
+                
+                if(mysqli_num_rows($result) > 0)
+                    return mysqli_fetch_array($result);
+            }
+            catch(Exception $e)
+            {
+                alert($e);
+            }
+            finally
+            {
+                mysqli_close($conStr);
+            }
+        }
+    }
+?>
