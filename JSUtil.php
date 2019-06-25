508ab4f9af108885a425c34c6279a1def752097b
Dipolelo <dipolelodips@gmail.com>
2019-06-25 10:44:27 +0200

    Added JS Utilities

---
+++ JSUtil.php
@@ -0,0 +1,12 @@
+
+<?php 
+    function alert($message)
+    {
+        echo "<script> alert($message) </script>";
+    }
+
+    function route($link)
+    {
+        echo "<script> window.location.href = '$link'</script>";
+    }
+?>
