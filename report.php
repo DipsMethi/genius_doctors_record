7c5b6848376524556190d02a4af9624ac274fc1e
Dipolelo <dipolelodips@gmail.com>
2019-06-26 15:05:57 +0200

    Add session manager

--- report.php
+++ report.php
@@ -8,11 +8,11 @@ $summary = $_POST['summary'];
 $prescription = $_POST['prescription'];*/
 
 require 'C:\xampp\htdocs\genius_doctors_record\fpdf.php';
-include 'Utilities/JSUtils.php';
+include ("Utilities/JSUtil.php");
+include_once ("Utilities/sessionManager.php");
 
     if(isset($_POST['report']))
     {
-
         try
         {
             $pdf=new FPDF();
@@ -24,37 +24,38 @@ include 'Utilities/JSUtils.php';
             // Connect to database
             $conn= mysqli_connect("localhost", "root", "", "doctors_db");
 
-            $query=mysqli_query($conn,"select * from patient_medical_record");
+            $idNum = $_SESSION['id'];
+            $query = mysqli_query($conn,"SELECT * from patient_medical_record WHERE idNum=$idNum");
             while($data=mysqli_fetch_array($query))
             {
-            $pdf->Cell(0,10,"Pateint file",1,1,"C");
+                $pdf->Cell(0,10,"Patient file",1,1,"C");
 
-            $pdf->Cell(50,10,"Blood Pressure ",1,0);
-            $pdf->Cell(0,10,$data['blood_pressure'],1,1);
-
-
-            $pdf->Cell(50,10,"Blood Sugar ",1,0);
-            $pdf->Cell(0,10,$data['blood_sugar'],1,1);
+                $pdf->Cell(50,10,"Blood Pressure ",1,0);
+                $pdf->Cell(0,10,$data['blood_pressure'],1,1);
 
 
-            $pdf->Cell(50,10,"Heart Rate ",1,0);
-            $pdf->Cell(0,10, $data['ht_rate'] ,1,1);
+                $pdf->Cell(50,10,"Blood Sugar ",1,0);
+                $pdf->Cell(0,10,$data['blood_sugar'],1,1);
 
 
-            $pdf->Cell(50,10,"Other ",1,0);
-            $pdf->Cell(0,10, $data['other'],1,1);
+                $pdf->Cell(50,10,"Heart Rate ",1,0);
+                $pdf->Cell(0,10, $data['ht_rate'] ,1,1);
 
 
-            $pdf->Cell(50,10,"Summary ",1,0);
-            $pdf->Cell(0,10, $data['summary'] ,1,1);
+                $pdf->Cell(50,10,"Other ",1,0);
+                $pdf->Cell(0,10, $data['other'],1,1);
 
 
-            $pdf->Cell(50,10,"Prescription ",1,0);
-            $pdf->Cell(0,10, $data['prescription'] ,1,1);
+                $pdf->Cell(50,10,"Summary ",1,0);
+                $pdf->Cell(0,10, $data['summary'] ,1,1);
 
-            $pdf->Cell(50,10,"ID Number ",1,0);
-            $pdf->Cell(0,10, $data['idNum'] ,1,1);
-            $pdf->Output();
+
+                $pdf->Cell(50,10,"Prescription ",1,0);
+                $pdf->Cell(0,10, $data['prescription'] ,1,1);
+
+                $pdf->Cell(50,10,"ID Number ",1,0);
+                $pdf->Cell(0,10, $data['idNum'] ,1,1);
+                $pdf->Output();
             }
         }
         catch(Exception $ex)
