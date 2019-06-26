18d865afdf9ff1ed66060e4ef860f1ad0fc20206
Dipolelo <dipolelodips@gmail.com>
2019-06-26 07:50:29 +0200

    Fix semantics

--- Utilities/sessionManager.php
+++ Utilities/sessionManager.php
@@ -1,43 +1,40 @@
 
 <?php
-    require_once "Utilities/JSUtil.php";
+    require_once 'JSUtil.php';
     session_start();
 
-    $id = $_POST['id'];
-    $pswd = $_POST['pswd'];
-    
-    try
+    $id = $_REQUEST['id'];
+    $pswd = $_REQUEST['pswd'];
+
+    if($con = mysqli_connect("localhost", "root", "", "doctors_db"))
     {
-        if($con = mysqli_connect("localhost", "root", "", "doctors_db"))
+        try
         {
-            $sql= "SELECT * FROM patient_profile where idNum=$id && pswd=$pswd";
+            $sql= "SELECT * FROM patient_profile where idNum='$id' && pswd='$pswd'";
             if($qry = mysqli_query($con, $sql))
-            {
-                $row = mysqli_num_rows($qry);
+            {        
                 $result = mysqli_fetch_array($qry);
+                $row = mysqli_num_rows($qry);
                 
                 if($row > 0)
                 {
-                    $_SESSION['id'] = $result['idNum'];
-                    $_SESSION['pswd'] = $result['pswd'];
+                    $_SESSION['id'] = $id;
+                    $_SESSION['pswd'] = $pswd;
                     $_SESSION['fstName'] = $result['fst_name'];
                     $_SESSION['lstName'] = $result['lst_name'];
                     $_SESSION['email'] = $result['email'];
                 }
-                else
-                {
-                    $row = 0;
-                }
             }
             else alert("Error querying the database. \nPlease ensure that a connection is established or the database exists.");       
-        } 		  
-    }
-    catch(Exception $e)
-    {
-        alert($e);
-    }
-    finally
-    {
-        mysqli_close($con);
-    }
- ?>
+        }
+        catch(Exception $e)
+        {
+            alert($e);
+        }
+        finally
+        {
+            mysqli_close($con);
+        }
+    } 		  
+    
+?> 
