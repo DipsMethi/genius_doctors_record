389e9116c1ad0f5a1c319faccf62062cb99c9901
Dipolelo <dipolelodips@gmail.com>
2019-07-02 11:10:51 +0200

    Separated from HTML

---
+++ php/Backend/PatientProfilePreprocessor.php
@@ -0,0 +1,70 @@
+<?php
+    include_once ("PHP/Utilities/JSUtil.php");
+    require_once ("PHP/Utilities/SessionManager.php");
+
+    try
+    {
+        $id = $_SESSION['id'];
+        $row = getProfile($id); // Gets profile of patient
+
+        if(isset($_POST['save']))
+        {
+            $pswd = $_POST['pswd'];
+            $cPassword = $_POST['cPswd'];
+            
+            if( $cPassword == $pswd )
+            {
+                updateProfile($id);    
+            } 
+            elseif ($pswd != $cPassword)
+            {
+                alert("Password mismatch.");
+            } 
+        }
+    }
+    catch(Exception $ex)
+    {
+        alert($ex->getMessage());
+    }
+
+    function getProfile($id)
+    {
+        try
+        {
+            $conStr = mysqli_connect("localhost","root","","doctors_db");
+            $query = "SELECT * FROM patient_profile WHERE idNum='$id'";
+            $result = mysqli_query($conStr,$query);
+            
+            if(mysqli_num_rows($result) > 0)
+                return mysqli_fetch_array($result);
+        }
+        catch(Exception $e)
+        {
+            alert( $e->getMessage() );
+        }
+        finally
+        {
+            mysqli_close($conStr);
+        }
+    }
+
+    function updateProfile($id)
+    {
+        $fstName = $_REQUEST['fstName'];
+        $lstName = $_REQUEST['lstName'];
+        $pswd = $_REQUEST['pswd'];
+        $email = $_REQUEST['email'];
+
+        $c = mysqli_connect("localhost","root", "", "doctors_db");
+        
+        $q = "UPDATE patient_profile SET fst_name ='$fstName'
+                                         ,lst_name ='$lstName'
+                                         ,pswd ='$pswd'
+                                         ,email ='$email'
+                                     WHERE idNum='$id'";
+        if(mysqli_query($c, $q))
+        {
+          route("patient_login.php");
+        }
+    }
+?>
